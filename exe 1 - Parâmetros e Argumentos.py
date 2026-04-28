baac15b58be8f09da23e482ca8018c9aef52b8ff
Pedro Henrique Silva dos Santos <pedro.henrique.phst112@gmail.com>
2026-04-28 15:22:49 -0300

    28/04/2026

---
+++ exe 1 - Parâmetros e Argumentos.py
@@ -0,0 +1,12 @@
+#Parâmetros com valor padrão (Default)
+def saudacao(nome,mensagem = "Bem-Viado!"):
+    print(f"Olá {nome}! {mensagem}")
+nome = input("Digite seu nome: ")
+saudacao(nome)
+
+#Argumentos nomeados (Keyword args)
+def criar_usuario(nome,idade, admin=False):
+    print(f"{nome} | {idade} anos |admin={admin}")
+nome_usuario = input("Digite seu nome: ")
+idade_usuario = input("Digite sua idade: ")
+criar_usuario(nome_usuario, idade_usuario)
